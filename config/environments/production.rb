03cbb70340fdcaf0dd17ca157dce856bacf0a010
Tanguy Billon <tanguy_billon@hotmail.fr>
2019-11-05 17:01:04 +0100

    up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { :host => 'eventtribethp.herokuapp.com' }
+
+  config.action_mailer.perform_deliveries = true
 end
