4c420b171abe18d500627456c753b850ee0e5929
Tanguy Billon <tanguy_billon@hotmail.fr>
2019-11-06 13:31:28 +0100

    abitbetterrr

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,7 +9,15 @@ class EventsController < ApplicationController
   end
 
   def create
-    @event = Event.find(params[:id])
+    @event = Event.new(user_id: current_user, title: params[:title], start_date: params[:start_date], duration: params[:duration], description: params[:description], price: params[:price], location: params[:location])
+		if @event.save
+			flash[:success] = "Event successfully created! 👍"
+			#Show events index
+			redirect_to '/'
+		else
+    	# Keeps on the new event view
+    	render 'new' 
+    end
   end
 
   def edit
